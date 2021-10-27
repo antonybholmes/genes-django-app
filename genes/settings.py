a75aa77f83ebbf28592b59b201eaf45b8c6f91b8
Antony Holmes <antony.b.holmes@gmail.com>
2021-10-27 18:30:04 -0400

    Updates and bug fixes.

--- genes/settings.py
+++ genes/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 import json
 
-config = json.load(open('genes/settings.json', 'r'))
+config = json.load(open('settings.json', 'r'))
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
